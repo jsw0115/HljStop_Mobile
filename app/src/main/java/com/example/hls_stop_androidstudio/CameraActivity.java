dba4c6550ed6c81889bbe861f450e1fbbf84f38a
Easy-Water71 <phllo154@naver.com>
2021-08-28 16:40:52 +0900

    feat

--- app/src/main/java/com/example/hls_stop_androidstudio/CameraActivity.java
+++ app/src/main/java/com/example/hls_stop_androidstudio/CameraActivity.java
@@ -6,14 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
@@ -24,16 +21,25 @@ import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.*;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.dnn.Dnn;
+import org.opencv.utils.Converters;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
@@ -49,6 +55,26 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     boolean firstTimeYolo = false;
     Net tinyYolo;
 
+    private static String getPath(String file, Context context){
+        AssetManager assetManager =context.getAssets();
+        BufferedInputStream inputStream = null;
+        try {
+            inputStream=new BufferedInputStream(assetManager.open(file));
+            byte[] data=new byte[inputStream.available()];
+            inputStream.read(data);
+            inputStream.close();
+            File outFile=new File(context.getFilesDir(),file);
+            FileOutputStream os=new FileOutputStream(outFile);
+            os.write(data);
+            os.close();
+            return outFile.getAbsolutePath();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     public void YOLO(View Button)
     {
         if(startYolo == false) {
@@ -56,8 +82,8 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
             if (firstTimeYolo == false) {
                 firstTimeYolo = true;
 
-                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
-                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+                String tinyYoloCfg = getPath("yolov3-tiny.cfg", this);
+                String tinyYoloWeights = getPath("yolov3-tiny.weights", this);
 
                 tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
             }
@@ -129,18 +155,97 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
         if(startYolo == true)
         {
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
-            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392,
-                    new Size(416, 416), new Scalar(0, 0, 0), false, false);
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0), false, false);
 
             tinyYolo.setInput(imageBlob);
-            tinyYolo.forward();
+
+            java.util.List<Mat> result = new java.util.ArrayList<Mat>(2);
+
+            List<String> outBlobNames = new java.util.ArrayList<>();
+            outBlobNames.add(0, "yolo_16");
+            outBlobNames.add(1, "yolo_23");
+
+            tinyYolo.forward(result, outBlobNames);
+
+            float confThreshold = 0.3f;
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect> rects = new ArrayList<>();
+
+            for(int i = 0; i < result.size(); ++i)
+            {
+                Mat level = result.get(i);
+                for(int j = 0; j < level.rows(); ++j)
+                {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+                    float confidence = (float)mm.maxVal;
+
+                    Point classIdPoint = mm.maxLoc;
+
+                    if(confidence > confThreshold)
+                    {
+                        int centerX = (int)(row.get(0,0)[0] * frame.cols());
+                        int centerY = (int)(row.get(0,1)[0] * frame.rows());
+                        int width = (int)(row.get(0,2)[0] * frame.cols());
+                        int height = (int)(row.get(0,3)[0] * frame.rows());
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
+
+                        clsIds.add((int)classIdPoint.x);
+                        confs.add((float)confidence);
+                        rects.add(new Rect(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+
+            if(ArrayLength >= 1)
+            {
+                float nmsThresh = 0.2f;
+
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+
+                Rect[] boxesArray = rects.toArray(new Rect[0]);
+                MatOfRect boxes = new MatOfRect(boxesArray);
+                MatOfInt indices = new MatOfInt();
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+                int[] ind = indices.toArray();
+                for(int i = 0; i < ind.length; ++i)
+                {
+                    int idx = ind[i];
+                    Rect box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
+
+                    int intConf = (int)(conf * 100);
+
+                    Imgproc.putText(frame, cocoNames.get(idGuy)+" "+intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(0, 255, 0), 2);
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+                }
+            }
         }
         return frame;
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        if(startYolo == true)
+        {
+            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/yolov3-tiny.cfg";
+            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/yolov3-tiny.weights";
 
+            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+        }
     }
 
     @Override
